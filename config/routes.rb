0f15d6dced52aac225019f8f1761c2f52ae18734
Christian Thomas <christian.thomas.2894@gmail.com>
2018-09-04 21:20:31 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'products/index'
-
-  get 'products/add'
   post 'products/add'
 
   # The priority is based upon order of creation: first created -> highest priority.
